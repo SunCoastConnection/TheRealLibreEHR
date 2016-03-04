eb8adfd8b7de2f6933ef0a4bf8f2d0fd4b65ee52
aethelwulffe <art@starfrontiers.org>
2016-03-04 13:06:41 -0500

    CAD IPP

--- library/classes/rulesets/PQRS/groups/PQRS_Group_CAD_0130/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_CAD_0130/InitialPatientPopulation.php
@@ -30,8 +30,9 @@ class PQRS_Group_CAD_0130_InitialPatientPopulation implements PQRSFilterIF
     
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	//Default return 
-        return false;
+require_once("$srcdir/classes/rulesets/PQRS/groups/common/CADcommon.php");
+$result = sqlStatement($CAD);
+if ($result > 0){ return true;} else {return false;}  
     }
 }
 
