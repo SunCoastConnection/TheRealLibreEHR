a8ae2f00c85e32435255c5ea2801593f4f913836
B Lee <leebc11@acm.org>
2016-03-04 12:23:38 -0500

    Created skeleton for Measure 0221

---
+++ library/classes/rulesets/PQRS/reports/PQRS_0221/Exclusion.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * PQRS Measure 0221 -- Exclusion 
+ *
+ * Copyright (C) 2016 Suncoast Connection
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  leebc
+ * @link    http://www.open-emr.org
+ */
+//
+class PQRS_0221_Exclusion implements PQRSFilterIF
+{
+    public function getTitle() 
+    {
+        return "Exclusion";
+    }
+    
+    public function test( PQRSPatient $patient, $beginDate, $endDate )
+    {
+       	// Default return 
+        return false;
+    }
+}
