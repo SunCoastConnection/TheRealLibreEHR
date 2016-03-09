2b92dc8d1a2c642359df3a7d300d042ce611e8d5
aethelwulffe <art@starfrontiers.org>
2016-03-09 16:25:30 -0500

    Surgery numerators

--- library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0130/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0130/Numerator.php
@@ -30,9 +30,19 @@ class PQRS_Group_General_Surgery_0130_Numerator implements PQRSFilterIF
 
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	// Otherwise Default return
-	return false;
-		
+$query =
+" SELECT COUNT(b1.code)".  
+" FROM billing AS b1".
+
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" WHERE b1.pid = '$Patient' ".
+" AND b1.user = '$Provider' ".
+" AND YEAR(fe.date) ='2015' ".
+" AND b1.code IN ('G8427', 'G8430' ) ; ";
+
+$result = sqlStatement($query); 
+
+if ($result > 0){ return true;} else {return false;}   				
     }
 }
 
--- library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0226/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0226/Numerator.php
@@ -30,9 +30,19 @@ class PQRS_Group_General_Surgery_0226_Numerator implements PQRSFilterIF
 
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	// Otherwise Default return
-	return false;
-		
+$query =
+" SELECT COUNT(b1.code)".  
+" FROM billing AS b1".
+
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" WHERE b1.pid = '$Patient' ".
+" AND b1.user = '$Provider' ".
+" AND YEAR(fe.date) ='2015' ".
+" AND b1.code IN ('4004F', '1036F' ) AND b1.modifier != '8P'; ";
+
+$result = sqlStatement($query); 
+
+if ($result > 0){ return true;} else {return false;}   		
     }
 }
 
