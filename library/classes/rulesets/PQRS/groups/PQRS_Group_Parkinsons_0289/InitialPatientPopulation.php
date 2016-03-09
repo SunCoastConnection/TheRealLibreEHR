5f7bc3d117b2a8e20e291d5d6bfc93b385b6c15a
aethelwulffe <art@starfrontiers.org>
2016-03-08 21:07:11 -0500

    Parkinsons Group IPP

--- library/classes/rulesets/PQRS/groups/PQRS_Group_Parkinsons_0289/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_Parkinsons_0289/InitialPatientPopulation.php
@@ -30,8 +30,9 @@ class PQRS_Group_Parkinsons_0289_InitialPatientPopulation implements PQRSFilterI
     
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	//Default return 
-        return false;
+require_once("$srcdir/classes/rulesets/PQRS/groups/common/Parkinsoncommon.php");
+$result = sqlStatement($Parkinson);
+if ($result > 0){ return true;} else {return false;}  
     }
 }
 
