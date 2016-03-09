b4904e5251120b5ebbf22fa2b4f1e539dd7e608b
aethelwulffe <art@starfrontiers.org>
2016-03-09 15:14:42 -0500

    COPD numerator

--- library/classes/rulesets/PQRS/groups/PQRS_Group_COPD_0052/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_COPD_0052/Numerator.php
@@ -30,8 +30,19 @@ class PQRS_Group_COPD_0052_Numerator implements PQRSFilterIF
 
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	// Otherwise Default return
-	return false;
+$query =
+" SELECT COUNT(b1.code)".  
+" FROM billing AS b1".
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" INNER JOIN billing AS b2 ON (b2.pid = b1.pid)".
+" WHERE b1.pid = '$Patient' ".
+" AND b1.user = '$Provider' ".
+" AND YEAR(fe.date) ='2015' ".
+" AND ((b1.code = '4025F' AND b1.modifier != '8P')AND b2.code = 'G8924' ); ";
+
+$result = sqlStatement($query); 
+
+if ($result > 0){ return true;} else {return false;}     
 		
     }
 }
