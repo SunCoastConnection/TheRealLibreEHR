1228e850878b36b82941ca032d82e6bfadde234d
B Lee <leebc11@acm.org>
2016-04-27 15:37:14 -0400

    Created skeleton for New 2016 PQRS Measure 0439

---
+++ library/classes/rulesets/PQRS/reports/PQRS_0439/InitialPatientPopulation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PQRS Measure 0439 -- Initial Patient Population
+ *
+ * Copyright (C) 2016      Suncoast Connection
+ *
+ * @package OpenEMR
+ * @link    http://suncoastconnection.com
+ * @author  Bryan lee <leebc 11 at acm dot org>
+ * @author  Suncoast Connection
+ */
+ 
+class PQRS_0439_InitialPatientPopulation implements PQRSFilterIF
+{
+    public function getTitle() 
+    {
+        return "Initial Patient Population";
+    }
+    
+    public function test( PQRSPatient $patient, $beginDate, $endDate )
+    {
+	//Default return 
+        return false;
+    }
+}
+
+?>
