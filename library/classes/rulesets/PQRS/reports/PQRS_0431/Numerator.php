9bb2869a037b52a722a0ef7571e02170450ff742
B Lee <leebc11@acm.org>
2016-04-27 15:37:12 -0400

    Created skeleton for New 2016 PQRS Measure 0431

---
+++ library/classes/rulesets/PQRS/reports/PQRS_0431/Numerator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PQRS Measure 0431 -- Numerator
+ *
+ * Copyright (C) 2016      Suncoast Connection
+ *
+ * @package OpenEMR
+ * @link    http://suncoastconnection.com
+ * @author  Bryan lee <leebc 11 at acm dot org>
+ * @author  Suncoast Connection
+ */
+ 
+class PQRS_0431_Numerator implements PQRSFilterIF
+{
+    public function getTitle()
+    {
+        return "Numerator";
+    }
+
+    public function test( PQRSPatient $patient, $beginDate, $endDate )
+    {
+	// Otherwise Default return
+	return false;
+		
+    }
+}
+
+?>
