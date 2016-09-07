b4056727c5dc68143d1617fc2b8ec8fa3f74b3e5
B Lee <leebc11@acm.org>
2016-09-07 11:31:38 -0400

    Changed queries to use variable-date not fixed-dates
    Changes:
    *   " AND YEAR(fe.date) ='2016' ".
    turns into
        " AND fe.date >=? ".
        " AND fe.date <=? ".
    AND
    *   array($patient->id), $beginDate)
    turns into
        array($patient->id), $beginDate, $endDate)
    
    Other variations depending on spaces.  (Some variations not caught and will
    
    * Reviewed changes and they look correct to me.
    
    SED script was (with manual review and confirmation of changes):
    
    sed -e 's/\"AND YEAR(fe.date)/\" AND YEAR(fe.date)/' \
        -e "s/AND YEAR(fe.date) ='2016'/AND YEAR(fe.date) =?/" \
        -e 's/) =?\"/) =? \"/' \
        -e 's/\" AND YEAR(fe.date) =? \"/\" AND fe.date >=? \".\n\" AND fe.date <=? \"/' \
        -e 's/AND YEAR(fe.date) >=?/AND fe.date >=?/' \
        -e 's/AND YEAR(fe.date) <=?/AND fe.date <=?/' \
        -e 's/sqlFetchArray(sqlFetchArray(/sqlFetchArray(/' \
        -e 's/array($patient->id),$beginDate)/array($patient->id), $beginDate)/' \
        -e 's/sqlStatementNoLog($query, array($patient->id)));/sqlStatementNoLog($query, array($patient->id), $beginDate));/' \
        -e 's/sqlFetchArray(sqlStatementNoLog($query, array($patient->id), $beginDate));/sqlFetchArray(sqlStatementNoLog($query, array($patient->id), $beginDate, $endDate));/'     < $INFILE > $TEMPFILE
    
     On branch PQRS/VariableDatesNotFixedDates-1
     Changes to be committed:
    	modified:   reports/PQRS_0005/Denominator.php
    	modified:   reports/PQRS_0008/InitialPatientPopulation.php
    	modified:   reports/PQRS_0022/Numerator.php

--- library/classes/rulesets/PQRS/reports/PQRS_0022/Numerator.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0022/Numerator.php
@@ -36,11 +36,12 @@ class PQRS_0022_Numerator implements PQRSFilterIF
 "JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 "INNER JOIN billing AS b2 ON (b2.pid = b1.pid)".  
 "WHERE b1.pid = ?  ".
-"AND YEAR(fe.date) =? ".
+" AND fe.date >=? ".
+" AND fe.date <=? ".
 "AND ".
 "((b1.code = '4049F' AND b1.modifier !='8P') AND b2.code= '4046F'); ";
 
-$result = sqlFetchArray(sqlStatementNoLog($query),array($patient->id), $beginDate); 
+$result = sqlFetchArray(sqlStatementNoLog($query,array($patient->id), $beginDate, $endDate)); 
 
 if ($result > 0){ return true;} else {return false;}     
 	
