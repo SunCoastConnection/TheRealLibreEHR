aa56db5ec8b67fb0797d627498cf56fa628b060e
Art Eaton <Art@starfrontiers.org>
2016-10-10 19:01:26 -0400

    Update headers
    Non-gpl for now.  Maybe MPL later. Much smaller.

--- library/classes/rulesets/PQRS/reports/PQRS_0236/Denominator.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0236/Denominator.php
@@ -3,23 +3,10 @@
  * PQRS Measure 0236 -- Denominator 
  *
  * Copyright (C) 2016      Suncoast Connection
- *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
- *
- * @package OpenEMR
- * @link    http://www.oemr.org
+ * @package PQRS_Gateway 
  * @link    http://suncoastconnection.com
- * @author  Bryan lee <leebc 11 at acm dot org>
- * @author  Art Eaton <art@starfrontiers.org>
+ * @author  Bryan lee <bryan@suncoastconnection.com>
+ * @author  Art Eaton <art@suncoastconnection.com>
  */
  
 class PQRS_0236_Denominator implements PQRSFilterIF
