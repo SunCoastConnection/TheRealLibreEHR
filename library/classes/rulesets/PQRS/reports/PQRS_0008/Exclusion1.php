8d2d259a4d5d6a8a6b998d9ca128d7653428f147
Bryan Lee <leebc11@acm.org>
2016-10-10 23:45:10 -0400

    Changes to measures.  Syntax errors, etc.
     On branch PQRS/measure_patches-20161010
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0007/InitialPatientPopulation1.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0008.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0008/Exclusion1.php

--- library/classes/rulesets/PQRS/reports/PQRS_0008/Exclusion1.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0008/Exclusion1.php
@@ -9,7 +9,7 @@
  * @author  Art Eaton <art@suncoastconnection.com>
  */
 
-class PQRS_0008_Exclusion implements PQRSFilterIF
+class PQRS_0008_Exclusion1 implements PQRSFilterIF
 {
     public function getTitle() 
     {
