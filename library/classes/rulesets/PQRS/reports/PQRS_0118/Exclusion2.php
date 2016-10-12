1ec4d29f2f4d52d45dd38f8cc3bfcccae36f762f
Art Eaton <Art@starfrontiers.org>
2016-10-12 15:21:26 -0400

    cleanup to 140

--- library/classes/rulesets/PQRS/reports/PQRS_0118/Exclusion2.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0118/Exclusion2.php
@@ -9,7 +9,7 @@
  * @author  Art Eaton <art@suncoastconnection.com>
  */
 
-class PQRS_0118_Exclusion implements PQRSFilterIF
+class PQRS_0118_Exclusion2 implements PQRSFilterIF
 {
     public function getTitle() 
     {
