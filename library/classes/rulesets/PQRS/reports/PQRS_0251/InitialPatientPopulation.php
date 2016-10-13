f2c4bbb42ad78df070752d51f524ddd03105818f
Art Eaton <Art@starfrontiers.org>
2016-10-13 10:45:56 -0400

    To 255

--- library/classes/rulesets/PQRS/reports/PQRS_0251/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0251/InitialPatientPopulation.php
@@ -18,7 +18,21 @@ class PQRS_0251_InitialPatientPopulation implements PQRSFilterIF
     
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	//Default return 
-        return false;
+$query =
+"SELECT COUNT(b1.code) as count ".  
+" FROM billing AS b1". 
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" INNER JOIN billing AS b2 ON (b2.pid = b1.pid)".
+" INNER JOIN pqrs_efcc AS codelist_a ON (b1.code = codelist_a.code)".
+" INNER JOIN pqrs_efcc AS codelist_b ON (b2.code = codelist_b.code)".
+" WHERE b1.pid = ? ".
+" AND fe.date >= '".$beginDate."' ".
+" AND fe.date <= '".$endDate."' ".
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0251_a') ".
+" AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_0251_b'); ";
+
+$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+if ($result['count']> 0){ return true;} else {return false;}  
+
     }
 }
