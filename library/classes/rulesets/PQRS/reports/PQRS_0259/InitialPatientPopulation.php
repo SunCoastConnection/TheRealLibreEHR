290c83a8b9787c3c6c5d1a8bed00651990aedd21
Art Eaton <Art@starfrontiers.org>
2016-10-13 13:59:36 -0400

    To 264

--- library/classes/rulesets/PQRS/reports/PQRS_0259/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0259/InitialPatientPopulation.php
@@ -18,7 +18,57 @@ class PQRS_0259_InitialPatientPopulation implements PQRSFilterIF
     
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	//Default return 
-        return false;
+$query =
+"SELECT COUNT(b1.code) as count ".  
+" FROM billing AS b1". 
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" INNER JOIN pqrs_ptsf AS codelist_a ON (b1.code = codelist_a.code)".
+" JOIN patient_data AS p ON (p.pid = b1.pid)".
+" WHERE b1.pid = ? ".
+" AND fe.date >= '".$beginDate."' ".
+" AND fe.date <= '".$endDate."' ".
+" AND TIMESTAMPDIFF(YEAR,p.dob,fe.date) >= '18' ".
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0259_a') ".
+
+$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+if ($result['count']> 0){
+	$query = 
+	"SELECT sex AS count".
+	" FROM patient_data".
+	" WHERE b1.pid = ? ;";
+	$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+	
+	if ($result['count']='Male'){ 
+								$query =
+								" SELECT COUNT(b1.code) AS count".  
+								" FROM billing AS b1".
+								" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+								" WHERE b1.pid = ? ".
+								" AND fe.date >= '".$beginDate."' ".
+								" AND fe.date <= '".$endDate."' ".
+								" AND NOT b1.code = '9004F'; ";
+								
+								$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id))); 
+								
+								if ($result['count']> 0){ return true;} else {return false;}  
+								
+								} else {
+									
+								$query =
+								" SELECT COUNT(b1.code) AS count".  
+								" FROM billing AS b1".
+								" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+								" WHERE b1.pid = ? ".
+								" AND fe.date >= '".$beginDate."' ".
+								" AND fe.date <= '".$endDate."' ".
+								" AND NOT b1.code IN ('9003F','9004F'); ";
+								
+								$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id))); 
+								
+								if ($result['count']> 0){ return true;} else {return false;}  
+		
+								}
+					  } else {return false;}  
     }
 }
+
