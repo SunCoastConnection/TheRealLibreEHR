73aab96a1105fb99b45bc1b7958266c21ef69866
Art Eaton <Art@starfrontiers.org>
2016-10-15 08:20:58 -0400

    Create Group Table

--- library/classes/rulesets/PQRS/groups/common/Apneacommon.php
+++ library/classes/rulesets/PQRS/groups/common/Apneacommon.php
@@ -13,14 +13,13 @@ $query =
 "SELECT COUNT(b1.code) as count". 
 "  FROM billing AS b1".  
 " INNER JOIN billing AS b2 ON (b1.pid = b2.pid)".  
-
+" INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
+" INNER JOIN pqrs_group AS codelist_b ON (b2.code = codelist_b.code)".
 " JOIN form_encounter AS fe ON (b2.encounter = fe.encounter)".
 " JOIN patient_data AS p ON (b1.pid = p.pid)". 
-" WHERE b1.pid = ? ".  
-" AND b1.code IN".  
-"('327.23', ' 780.51', ' 780.53', ' 780.57', ' G47.30', ' G47.33')".
-" AND YEAR(fe.date) ='2016' ".
+" WHERE b1.pid = ? ".
+" AND fe.date BETWEEN ('".$beginDate."' AND '".$endDate."') ".
 " AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '18'  ".  
-" AND b2.code IN". 
-"('99201', ' 99202', ' 99203', ' 99204', ' 99205', ' 99212', ' 99213', ' 99214', ' 99215', ' 99304', ' 99305', ' 99306', ' 99307', ' 99308', ' 99309', ' 99310', ' 99324', ' 99325', ' 99326', ' 99327', ' 99328', ' 99334', ' 99335', ' 99336', ' 99337', ' 99341', ' 99342', ' 99343', ' 99344', ' 99345', ' 99347', ' 99348', ' 99349', ' 99350');";
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_apnea_a') ".
+" AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_apnea_b');";
 ?>
