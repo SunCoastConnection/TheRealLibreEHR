d2c9ce7ec8f2f159b9eaf6990118a5a902040a21
B Lee <leebc11@acm.org>
2016-10-21 03:36:30 -0400

    Changed second "WHERE" to "AND"
    	modified:   groups/PQRS_Group_Preventive_0039/Denominator.php
    	modified:   groups/PQRS_Group_Preventive_0048/Denominator.php
    	modified:   groups/PQRS_Group_Preventive_0112/Denominator.php

--- library/classes/rulesets/PQRS/groups/PQRS_Group_Preventive_0048/Denominator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_Preventive_0048/Denominator.php
@@ -23,7 +23,7 @@ class PQRS_Group_Preventive_0048_Denominator implements PQRSFilterIF
 $query =
 "SELECT COUNT(p.pid) as count ".  
 " FROM  patient_data AS p WHERE (p.pid = ?)".
-" WHERE p.sex = 'Female'".
+" AND p.sex = 'Female'".
 " AND TIMESTAMPDIFF(YEAR,p.DOB,'".$endDate."') >='65'; ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
