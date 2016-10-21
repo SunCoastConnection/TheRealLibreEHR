f8ff0a733364fdad0f55f8a2c5268ad0073966a0
Art Eaton <Art@starfrontiers.org>
2016-10-20 20:26:26 -0400

    cp236

--- library/classes/rulesets/PQRS/groups/PQRS_Group_CP_0236/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_CP_0236/Numerator.php
@@ -19,8 +19,17 @@ class PQRS_Group_CP_236_Numerator implements PQRSFilterIF
 
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	// Otherwise Default return
-	return false;
+"SELECT COUNT(b1.code) as count ".  
+" FROM billing AS b1". 
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" JOIN patient_data AS p ON (p.pid = b1.pid)".
+" WHERE b1.pid = ? ".
+" AND fe.date BETWEEN ('".$beginDate."' AND '".$endDate."') ".
+" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) BETWEEN ('18' AND '85') ".
+" AND b1.code  IN ('G8752','G8754'); ";
+
+$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+if ($result['count']> 0){ return true;} else {return false;} 
 		
     }
 }
