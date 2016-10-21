3493767327bcc83ee7b2f0639e66ca35432a4734
Art Eaton <Art@starfrontiers.org>
2016-10-20 21:11:26 -0400

    MCC and I am done

--- library/classes/rulesets/PQRS/groups/PQRS_Group_MCC_0154/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_MCC_0154/Numerator.php
@@ -19,8 +19,18 @@ class PQRS_Group_MCC_0154_Numerator implements PQRSFilterIF
 
     public function test( PQRSPatient $patient, $beginDate, $endDate )
     {
-	// Otherwise Default return
-	return false;
+$query =
+" SELECT COUNT(b1.code) as count ".  
+" FROM billing AS b1".
+" INNER JOIN billing AS b2 ON (b2.pid = b1.pid)".
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+" WHERE b1.pid = ? ".
+" AND fe.date BETWEEN ('".$beginDate."' AND '".$endDate."') ".
+" AND b1.code = '3288F' AND b2.code = '1100F'; ";
+
+$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+
+if ($result['count'] > 0){ return true;} else {return false;}  
 		
     }
 }
