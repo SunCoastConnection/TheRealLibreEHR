818c9797d13f4d84d1dbe70529e95aefe462f010
Art Eaton <Art@starfrontiers.org>
2016-10-21 02:37:50 -0400

    BETWEEN fixes

--- library/classes/rulesets/PQRS/groups/PQRS_Group_DR_0226/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_DR_0226/Numerator.php
@@ -24,7 +24,7 @@ $query =
 " FROM billing AS b1".
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " WHERE b1.pid = ? ".
-" AND fe.date BETWEEN ((DATE_SUB('".$beginDate."', INTERVAL 1 YEAR)) AND '".$endDate."') ".
+" AND fe.date BETWEEN DATE_SUB('".$beginDate."', INTERVAL 1 YEAR) AND '".$endDate."' ".
 " AND ( (b1.code = '4004F' AND b1.modifier ='') OR b1.code = '1036F'); ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
