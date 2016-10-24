41f593a19f0e00efd1e15f054d82cdf9c3b68eab
Art Eaton <Art@starfrontiers.org>
2016-10-24 17:46:45 -0400

    preventive431 and GS354

--- library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0354/Numerator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_General_Surgery_0354/Numerator.php
@@ -24,7 +24,7 @@ $query =
 " SELECT COUNT(b1.code) as count".  
 " FROM billing AS b1".
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
-" WHERE b1.pid = '$Patient' ".
+" WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 " AND b1.code = 'G9306'; ";
 
