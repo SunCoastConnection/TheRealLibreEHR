fd8a35830f03152af8a0ca0439e950a6d6aec3de
Art Eaton <Art@starfrontiers.org>
2016-10-24 16:13:11 -0400

    cataract191 correction

--- library/classes/rulesets/PQRS/groups/PQRS_Group_Cataracts_0191/Denominator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_Cataracts_0191/Denominator.php
@@ -24,10 +24,10 @@ $query =
 "SELECT COUNT(b1.code) as count ".  
 " FROM billing AS b1". 
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
-" INNER JOIN pqrs_efcc AS codelist_a ON (b1.code = codelist_b.code)".
+" INNER JOIN pqrs_efcc AS codelist_a ON (b1.code = codelist_a.code)".
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND (b1.code = codelist_b.code AND codelist_b.type = 'pqrs_0191_b'); ";
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0191_b'); ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
 if ($result['count']> 0){ return false;} else {return true;}
