ec4813873d7875f1099d7af09bdac3cf9ee5356e
B Lee <leebc11@acm.org>
2016-10-24 14:39:15 -0400

    Added missing ) at end of query
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0012/Exclusion.php

--- library/classes/rulesets/PQRS/reports/PQRS_0012/Exclusion.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0012/Exclusion.php
@@ -24,7 +24,7 @@ class PQRS_0012_Exclusion implements PQRSFilterIF
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ". 
-" AND (b1.code = '2027F' AND b1.modifier ='1P';" ;
+" AND (b1.code = '2027F' AND b1.modifier ='1P');" ;
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
 if ($result['count']> 0){ return true;} else {return false;}    
 
