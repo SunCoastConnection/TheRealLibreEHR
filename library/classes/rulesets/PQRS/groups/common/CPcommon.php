212918621d37b2b19a85e7583bf0845c97a13f2f
Art Eaton <Art@starfrontiers.org>
2016-10-24 17:08:50 -0400

    fix CP common

--- library/classes/rulesets/PQRS/groups/common/CPcommon.php
+++ library/classes/rulesets/PQRS/groups/common/CPcommon.php
@@ -13,14 +13,11 @@ $query =
 "SELECT COUNT(b1.code) as count".  
 "  FROM billing AS b1". 
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
-" INNER JOIN billing as b2 ON (b2.pid = b1.pid)".
 " JOIN patient_data AS p ON (p.pid = b1.pid)".
 " INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
-" INNER JOIN pqrs_group AS codelist_b ON (b2.code = codelist_b.code)".
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '18' ".
-" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_CP_a') ".
-" AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_CP_b');";
+" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '21' ".
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_CP_a');";
 
 ?>
