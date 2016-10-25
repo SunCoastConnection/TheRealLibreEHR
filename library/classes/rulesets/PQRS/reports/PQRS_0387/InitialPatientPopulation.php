3a83df11196265cf446f1e2a595c225ada7af06d
Art Eaton <Art@starfrontiers.org>
2016-10-25 15:16:45 -0400

    387 IPP brackets

--- library/classes/rulesets/PQRS/reports/PQRS_0387/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0387/InitialPatientPopulation.php
@@ -52,9 +52,9 @@ if ($result['count']> 0){
 					" AND b1.code IN( '99201', '99202', '99203', '99204', '99205', '99212','99213', '99214', '99215', '99304', '99305', '99306', '99307', '99308', '99309', '99310', '99324', '99325', '99326', '99327','99328', '99334', '99335', '99336', '99337', '99341', '99342', '99343', '99344', '99345', '99347', '99348', '99349', '99350') ; ";
 					
 					$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id))); 
-					if ($result['count']> 2){ return true;} else{return false; }  
+					if ($result['count']> 2){ return true;} else{return false; } } 
 
     }
-}
 
+}
 ?>
