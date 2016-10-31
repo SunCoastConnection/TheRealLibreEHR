b5f7169078cfb16cf4e6b141342a1063c6542348
Art Eaton <Art@starfrontiers.org>
2016-10-31 16:19:45 -0400

    turbo asthma
    Don't think this will help.

--- library/classes/rulesets/PQRS/groups/common/Asthmacommon.php
+++ library/classes/rulesets/PQRS/groups/common/Asthmacommon.php
@@ -10,16 +10,30 @@
  */
 
 $query =
-"SELECT COUNT(b1.code) as count". 
-"  FROM billing AS b1".  
-" INNER JOIN billing AS b2 ON (b1.pid = b2.pid)".  
-" INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
-" INNER JOIN pqrs_group AS codelist_b ON (b2.code = codelist_b.code)".
-" JOIN form_encounter AS fe ON (b2.encounter = fe.encounter)".
-" JOIN patient_data AS p ON (b1.pid = p.pid)". 
-" WHERE b1.pid = ? ".  
-" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '5'  ".  
-" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_asthma_a') ".
-" AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_asthma_b');";
+		"SELECT COUNT(b1.code) as count ".  
+		"  FROM billing AS b1". 
+		" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
+		" JOIN patient_data AS p ON (p.pid = b1.pid)".
+		" INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
+		" WHERE b1.pid = ? ".
+		" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
+		" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '5' ".
+		" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_asthma_a');";
+		
+		$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+			if ($result['count']> 0){  
+	
+	    
+				$query =
+				"SELECT COUNT(b1.code) as count ".  
+				" FROM billing AS b1". 
+				" INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
+				" WHERE b1.pid = ? ".
+				" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_asthma_b');";
+				
+				$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
+				if ($result['count']> 0){ return true;} else {return false;}  
+						}	else {return false;}
 ?>
+				
+			
