aaf1ed7f0e7e4622ae52f03931034f04f50fa173
Art Eaton <Art@starfrontiers.org>
2016-11-08 15:30:16 -0500

    438_deidiotification
    #107

--- library/classes/rulesets/PQRS/reports/PQRS_0438/Exclusion2.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0438/Exclusion2.php
@@ -25,6 +25,9 @@ $query =
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 " AND b1.code ='G9667' ; ";
+$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id))); 
+
+if ($result['count']> 0){ return true;} else {return false;} 
     }
 }
 
