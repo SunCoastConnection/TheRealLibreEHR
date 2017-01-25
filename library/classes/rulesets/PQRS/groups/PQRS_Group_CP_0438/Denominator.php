4cf12f94f4e189ffe9f0477db3832715ad200c87
Art Eaton <Art@starfrontiers.org>
2017-01-25 14:02:32 -0500

    CP_438 add code to 3rd criteria
    Added diabetes check for 3rd criteria denom.

--- library/classes/rulesets/PQRS/groups/PQRS_Group_CP_0438/Denominator.php
+++ library/classes/rulesets/PQRS/groups/PQRS_Group_CP_0438/Denominator.php
@@ -39,12 +39,13 @@ class PQRS_Group_CP_0438_Denominator extends PQRSFilter
 							" FROM billing AS b1". 
 							" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 							" JOIN patient_data AS p ON (p.pid = b1.pid)".
-							" INNER JOIN pqrs_efcc5 AS codelist_a ON (b1.code = codelist_a.code)".
+							" JOIN billing AS b2 ON (b2.pid = b1.pid)".
+							" INNER JOIN pqrs_efcc5 AS codelist_a ON (b2.code = codelist_a.code)".
 							" WHERE b1.pid = ? ".
 							" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 							" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) BETWEEN '40' AND '75'  ".
-							" AND b1.code = 'G9666';".
-							
+							" AND b1.code = 'G9666' ".
+							" AND (b2.code = codelist_a.code AND codelist_a.type = 'pqrs_0438_b');
 							
 							$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
 							if ($result['count']> 0){ $counted=1;} else {$counted=0;} }	    
