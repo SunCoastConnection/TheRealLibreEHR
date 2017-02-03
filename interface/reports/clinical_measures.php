92f1a2015d851f6af58a44a340d33ab689368930
B Lee <leebc11@acm.org>
2017-02-03 16:38:25 -0500

    Added hook, refining output
    	modified:   interface/reports/clinical_measures.php
    	new file:   interface/reports/generate_pqrs_xml.php

--- interface/reports/clinical_measures.php
+++ interface/reports/clinical_measures.php
@@ -624,7 +624,7 @@ function Form_Validate() {
                           <?php echo htmlspecialchars(xl('Print'), ENT_NOQUOTES); ?>
                         </span>
                       </a>
-<?php   if(in_array($type_report, array('pqrs_individual_2015', 'pqrs_individual_2016'))) { ?>
+<?php   if(in_array($type_report, array('pqrs_individual_2016', 'pqrs_group_2016'))) { ?>
                       <a href="#" id="genPQRS" class='css_button' onclick='return downloadQRDA()'>
                         <span>
                           <?php echo htmlspecialchars(xl('Generate PQRS 2015 STUB'), ENT_NOQUOTES); ?>
@@ -633,6 +633,11 @@ function Form_Validate() {
                       <a href="#" id="xmlc_button" class='css_button' onclick='return GenXml("PQRS-3")'>
                         <span>
                           <?php echo htmlspecialchars(xl('Generate PQRS 2016 STUB (GenXml reference)'), ENT_NOQUOTES); ?>
+                        </span>
+                      </a>
+                      <a href="interface//reports/generate_pqrs_xml.php" id="xmlc_button" class='css_button' >
+                        <span>
+                          <?php echo htmlspecialchars(xl('Generate PQRS 2016 (Bryan test)'), ENT_NOQUOTES); ?>
                         </span>
                       </a>
 <?php   } elseif($type_report == 'cqm_2014') { ?>
