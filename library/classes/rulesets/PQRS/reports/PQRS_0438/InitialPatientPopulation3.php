ecf28be2563ec8325015d1c3ed2dc76876d40641
Bryan Lee <leebc11@acm.org>
2017-02-06 18:48:10 -0500

    Moved the DOB check back to the first part
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation1.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation2.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation3.php

--- library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation3.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation3.php
@@ -24,6 +24,7 @@ $query =
 " FROM billing AS b1". 
 " JOIN patient_data AS p ON (p.pid = b1.pid)".
 " WHERE b1.pid = ? ".
+" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) BETWEEN '45' AND '75' ".
 " AND b1.code = 'G9666'; ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
@@ -35,7 +36,6 @@ if ($result['count']> 0){
 	" INNER JOIN pqrs_efcc5 AS codelist_a ON (b1.code = codelist_a.code)".
 	" WHERE b1.pid = ? ".
 	" AND fe.provider_id = '".$this->_reportOptions['provider']."'".
-	" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) BETWEEN '45' AND '75' ".
 	" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 	" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0438_a'); ";
 	
