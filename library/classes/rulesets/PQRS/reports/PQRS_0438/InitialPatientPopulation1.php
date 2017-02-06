18bce95c2ccd3607d0ff886f65efe4b61bf2fe99
Bryan Lee <leebc11@acm.org>
2017-02-06 18:41:13 -0500

    Removed prov_id check from 1st, moved DOB to 2nd
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation1.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation2.php
    	modified:   library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation3.php

--- library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation1.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0438/InitialPatientPopulation1.php
@@ -22,11 +22,8 @@ class PQRS_0438_InitialPatientPopulation1 extends PQRSFilter
 $query =
 "SELECT COUNT(b1.code) as count ".  
 " FROM billing AS b1". 
-" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " JOIN patient_data AS p ON (p.pid = b1.pid)".
 " WHERE b1.pid = ? ".
-" AND fe.provider_id = '".$this->_reportOptions['provider']."'".
-" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '21' ".
 " AND b1.code = 'G9662'; ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
@@ -37,7 +34,8 @@ if ($result['count']> 0){
 	" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 	" INNER JOIN pqrs_efcc5 AS codelist_a ON (b1.code = codelist_a.code)".
 	" WHERE b1.pid = ? ".
-    " AND fe.provider_id = '".$this->_reportOptions['provider']."'".
+	" AND fe.provider_id = '".$this->_reportOptions['provider']."'".
+	" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '21' ".
 	" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 	" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0438_a'); ";
 	
