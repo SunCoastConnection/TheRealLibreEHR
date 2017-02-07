c674f13fc1df93f6a91d2c534fcd594af26b850c
Art Eaton <Art@starfrontiers.org>
2017-02-06 21:50:10 -0500

    Yet another fix for 5
    *sigh*

--- library/classes/rulesets/PQRS/reports/PQRS_0005/Numerator1.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0005/Numerator1.php
@@ -25,7 +25,7 @@ class PQRS_0005_Numerator1 extends PQRSFilter
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND (b1.code = '4010F' AND b1.modifier ='');" ;
+" AND b1.code = '4010F' AND b1.modifier ='';" ;
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
 if ($result['count']> 0){ return true;} else {return false;}  
 
