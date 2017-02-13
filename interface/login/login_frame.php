8a4759f193b49e91002fe091caa5f38e5678ed3f
Art Eaton <Art@starfrontiers.org>
2017-02-13 17:07:47 -0500

    mycatalyst theme
    Globals frameset stomps on anything you do in themes.  This is a
    workaround.

--- interface/login/login_frame.php
+++ interface/login/login_frame.php
@@ -12,7 +12,7 @@ include_once ("../globals.php");
 
 </HEAD>
 
-<frameset rows="<?php echo "$GLOBALS[logoBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
+<frameset rows="<?php if ($GLOBALS[$currenttheme]=='style_mycatalyst.css'){echo "162,$GLOBALS[titleBarHeight]"; }else{echo "$GLOBALS[logoBarHeight],$GLOBALS[titleBarHeight]";} ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
   <frame class="logobar" src="<?php echo $rootdir;?>/login/filler.php" name="Filler Top" scrolling="no" noresize frameborder="NO">
   <frame class="titlebar" src="<?php echo $rootdir;?>/login/login_title.php" name="Title" scrolling="no" noresize frameborder="NO">
   <frame src="<?php echo $rootdir;?>/login/login.php" name="Login" scrolling="auto" frameborder="NO">
