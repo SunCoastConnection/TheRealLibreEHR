9c14ac610a06acd54c3f53857f5a5f0f2334e2c1
Art Eaton <Art@starfrontiers.org>
2017-03-13 17:52:06 -0400

    remove false positives.
    removing IPP2 return true when no G code needs checking.  Set
    return=false to ensure no results.

--- library/classes/rulesets/PQRS/reports/pre_0224/InitialPatientPopulation2.php
+++ library/classes/rulesets/PQRS/reports/pre_0224/InitialPatientPopulation2.php
@@ -18,20 +18,7 @@ class pre_0224_InitialPatientPopulation2 extends preFilter
     
     public function test( prePatient $patient, $beginDate, $endDate )
     {
-$query =
-"SELECT COUNT(b1.code) as count ".  
-" FROM billing AS b1". 
-" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
-" INNER JOIN billing AS b2 ON (b2.pid = b1.pid)".
-" INNER JOIN pqrs_efcc3 AS codelist_a ON (b1.code = codelist_a.code)".
-" WHERE b1.pid = ? ".
-" AND fe.provider_id = '".$this->_reportOptions['provider']."'".
-" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0224_b') ".
-" AND b2.code = 'Z85.820';";
-
-$result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
-if ($result['count']> 1){ return true;} else {return false;}  
+return false; 
 
     }
 }
