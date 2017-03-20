8183a1393e48490c7f9ce521b71f4497648910d3
Art Eaton <Art@starfrontiers.org>
2017-03-20 17:02:33 -0400

    Mo-beddah-heddah

--- interface/main/PQRS/import_data.php
+++ interface/main/PQRS/import_data.php
@@ -1,24 +1,13 @@
 <?php
 /*
- *Claims2OEMR import script
+ *Claims2PQRS import script
  *
  * Copyright (C) 2016      Suncoast Connection
  *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
- *
- * @package OpenEMR
- * @link    http://www.oemr.org
+
+ * @package PQRS_Gateway
  * @link    http://suncoastconnection.com
- * @author  Suncoast ConnectionC:\AlphaFiles\bidenis\REPOS\PQRS_Gateway\sites\default\PQRS\dropzone\files\Importer
+ * @author  Suncoast Connection Inc.
 */
 require_once("../../globals.php");
 include_once("$srcdir/api.inc");
