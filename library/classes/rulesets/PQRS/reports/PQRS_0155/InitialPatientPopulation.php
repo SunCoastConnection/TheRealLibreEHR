e0641f74fafcf35aa2bbfe9f85f078ff424847db
Art Eaton <Art@starfrontiers.org>
2017-03-20 14:24:41 -0400

    fix 155
    Somehow committed a format change for 154 directly to the master.  Not
    worth reverting.  This commit actually removes the check against a
    non-existing code list in 155.

--- library/classes/rulesets/PQRS/reports/PQRS_0155/InitialPatientPopulation.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0155/InitialPatientPopulation.php
@@ -25,14 +25,12 @@ $query =
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " JOIN patient_data AS p ON (p.pid = b1.pid)".
 " INNER JOIN pqrs_ccco AS codelist_a ON (b1.code = codelist_a.code)".
-" INNER JOIN pqrs_ccco AS codelist_b ON (b2.code = codelist_b.code)".
-	" WHERE b1.pid = ? ".
-    " AND fe.provider_id = '".$this->_reportOptions['provider']."'".
+" WHERE b1.pid = ? ".
+" AND fe.provider_id = '".$this->_reportOptions['provider']."'".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
 " AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '65' ".
-" AND b3.code = '1100F' AND b3.modifier = '' ".
-" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0155_a')".
-" AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_0155_b'); ";
+" AND b2.code = '1100F' AND b2.modifier = '' ".
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0155_a'); ";
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
 if ($result['count']> 0){ return true;} else {return false;}  
 
