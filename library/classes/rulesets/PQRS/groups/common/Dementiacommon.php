237faeac32ddcfd13a7d10566a6ec7c2fba1be52
Art Eaton <Art@starfrontiers.org>
2017-03-25 16:42:25 -0400

    No age requiement
    There is no blanket age requirement for Dementia Group

--- library/classes/rulesets/PQRS/groups/common/Dementiacommon.php
+++ library/classes/rulesets/PQRS/groups/common/Dementiacommon.php
@@ -15,11 +15,9 @@ $query =
 " INNER JOIN billing AS b2 ON (b1.pid = b2.pid)". 
 " INNER JOIN pqrs_group AS codelist_a ON (b1.code = codelist_a.code)".
 " INNER JOIN pqrs_group AS codelist_b ON (b2.code = codelist_b.code)". 
-" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
-" JOIN patient_data AS p ON (b1.pid = p.pid)". 
+" JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)". 
 " WHERE b1.pid = ? ".  
-" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) >= '18'  ".  
+" AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ". 
 " AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_dementia_a') ".
 " AND (b2.code = codelist_b.code AND codelist_b.type = 'pqrs_dementia_b');";
 ?>
