61c1354ad6ea2fd3d88f22e6472bba201acffb3c
Art Eaton <Art@starfrontiers.org>
2018-02-01 16:14:08 -0500

    Measure 47 odd results fix
    unnested parens giving bad results.  Still cannot confirm why it matters
    at all, but this verifies the fix.

--- library/classes/rulesets/PQRS/reports/PQRS_0047/Numerator.php
+++ library/classes/rulesets/PQRS/reports/PQRS_0047/Numerator.php
@@ -32,7 +32,7 @@ $query =
 " JOIN form_encounter AS fe ON (b1.encounter = fe.encounter)".
 " WHERE b1.pid = ? ".
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND ( b1.code = '1123F' AND b1.modifier ='') OR( b1.code = '1124F' ); ";
+" AND (( b1.code = '1123F' AND b1.modifier ='') OR( b1.code = '1124F' )); ";
 //1123F-8P hard fail
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id))); 
 
