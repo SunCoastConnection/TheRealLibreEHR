692be682834791e615fd5a313b874a092b261289
Art Eaton <Art@starfrontiers.org>
2019-02-06 13:08:18 -0500

    Fix 463

--- modules/MIPS/rulesets/PQRS/reports/pre_0463/InitialPatientPopulation.php
+++ modules/MIPS/rulesets/PQRS/reports/pre_0463/InitialPatientPopulation.php
@@ -37,7 +37,7 @@ $query =
         " AND fe.provider_id = '".$this->_reportOptions['provider']."'";}
         $query .=
 " AND fe.date BETWEEN '".$beginDate."' AND '".$endDate."' ".
-" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0424_a'); "; 
+" AND (b1.code = codelist_a.code AND codelist_a.type = 'pqrs_0424_a') "; 
 " AND TIMESTAMPDIFF(YEAR,p.DOB,fe.date) BETWEEN '3' AND '17' ; ";
 
 $result = sqlFetchArray(sqlStatementNoLog($query, array($patient->id)));
