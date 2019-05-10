ae978999fb597574870f0665bfd409e1e3232b88
Terry Hill <teryhill@yahoo.com>
2019-05-10 14:04:45 -0400

    Provider scroll added
    Provider scroll added

--- modules/calendar/index.php
+++ modules/calendar/index.php
@@ -43,6 +43,20 @@ require('includes/session.php');
   <script src='fullcalendar-rightclick-1.9/fullcalendar-rightclick.js'></script>
   <link rel="stylesheet" href="../../assets/fonts/font-awesome-5-8-1/css/fontawesome.min.css" type="text/css">
   <style type="text/css">
+  /* style to provide horizontal scroll in Calendar's agenda views
+     by making table overflow its container */
+    .fc-view-container {
+        width: auto;
+    }
+
+    .fc-agenda-view {
+        overflow-x: scroll;
+    }
+
+    .fc-agenda-view > table {
+        width: var(--col-width, 1200px);
+        overflow-wrap: break-word;
+    }
     .show {
       display: block;
       background-color: #fff;
@@ -293,6 +307,30 @@ require('includes/session.php');
         calView.options.scrollTime = currCalTime;  // set scrollTime to current time
       }
 
+      function providerScroll() {
+        // Calendar tab's frame width - inner content + margin + space b/w frames
+        var calendarFrameWidth = $("#sidebar").width() + $("#calendar-container").width() + 16 + 7;
+        // setting height of the view area of the calendar so that horizontal scrollbar is visible
+        if (calendarFrameWidth > 580) {
+          $('#calendar').fullCalendar('option', 'contentHeight', 400);
+        } else if (calendarFrameWidth > 430) {
+          $('#calendar').fullCalendar('option', 'contentHeight', 370);
+        } else if (calendarFrameWidth > 390) {
+          $('#calendar').fullCalendar('option', 'contentHeight', 340);
+        } else {
+          $('#calendar').fullCalendar('option', 'contentHeight', 280);
+        }
+      }
+
+      function resizeAgendaViewTable(providers) {
+        // to change width of agenda view table based on number of providers
+        if (providers > 10) {
+          var toWidth = 120*providers; // taking ratio as 1200px for 10 providers
+          var bodyStyles = document.body.style;
+          bodyStyles.setProperty("--col-width", toWidth);
+        }
+      }
+
       $('#calendar').fullCalendar({
         schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
         locale: lang_default,
@@ -311,6 +349,7 @@ require('includes/session.php');
             }
             ?>
             type: 'agenda',
+            nowIndicator: true,
             duration: { days: 7 },
             buttonText: title_week,
             allDaySlot: false,
@@ -328,6 +367,7 @@ require('includes/session.php');
  //         },
           providerAgenda2Day: {
             type: 'agenda',
+            nowIndicator: true,
             duration: { days: 2 },
             buttonText: title_agenda2,
             allDaySlot: false,
@@ -692,13 +732,22 @@ require('includes/session.php');
             $('#datepicker').datetimepicker({ value: view.intervalStart.format() });
 
             scrollCalTime(view);  // when view changes or any date navigation method (prev, next, today) is called
+            var providers = $(".fc-resource-cell").length // number of provider column in agenda views
+            resizeAgendaViewTable(providers);
         },
         loading: function(isLoading, view) {
             // triggered when event or resource fetching starts/stops.
             if(isLoading) {
                 // fetching starts
                 scrollCalTime(view);  // when Calendar is loaded or refreshed
+            } else {
+                // fetching stops
+                providerScroll()  // make sure horizontal scroll bar is visible when Calendar info. is changed
             }
+        },
+        windowResize: function(view) {
+            // triggered when Calendar’s dimensions (frame width) changes due to opening/closing of other frames
+            providerScroll()  // make sure horizontal scroll bar is visible when frame width changes
         }
       }) //end calendar stuff
 
