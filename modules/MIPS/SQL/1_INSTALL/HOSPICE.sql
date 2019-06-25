0b49bfb71e6bfffe6616e97365de0a94ccb5f0d5
Art Eaton <Art@starfrontiers.org>
2019-06-25 16:20:13 -0400

    Add NotMET
    Also adding complete list of HOSPICE codes, and new Direct Entry table.

--- modules/MIPS/SQL/1_INSTALL/HOSPICE.sql
+++ modules/MIPS/SQL/1_INSTALL/HOSPICE.sql
@@ -1,4 +1,33 @@
-INSERT INTO HOSPICE (`code`) VALUES
+<?php
+/* Copyright (C) 2019    Suncoast Connection
+ * 
+ * LICENSE: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0
+ * See the Mozilla Public License for more details. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * @author  Art Eaton <art@suncoastconnection.com>
+ * @package LibreEHR 
+ * @link    http://suncoastconnection.com
+ *
+ * Please support this product by sharing your changes with the LibreEHR.org community.
+ */
+
+
+$query =
+"DROP TABLE IF EXISTS MIPS_hospice;";
+sqlStatementNoLog($query);
+
+$query =
+"CREATE TABLE `MIPS_hospice` (
+id int NOT NULL auto_increment,
+code varchar(15),
+PRIMARY KEY  (`id`)
+);";
+sqlStatementNoLog($query);
+
+$query =
+"INSERT INTO HOSPICE (`code`) VALUES
+('G9330'),
 ('G9687'),
 ('G9688'),
 ('G9690'),
@@ -7,7 +36,7 @@ INSERT INTO HOSPICE (`code`) VALUES
 ('G9693'),
 ('G9694'),
 ('G9700'),
-('G7902'),
+('G9702'),
 ('G9707'),
 ('G9710'),
 ('G9713'),
@@ -18,6 +47,18 @@ INSERT INTO HOSPICE (`code`) VALUES
 ('G9725'),
 ('G9740'),
 ('G9741'),
-('G9330'),
 ('G9747'),
-('G9749');
+('G9749'),
+('G9758'),
+('G9760'),
+('G9761'),
+('G9768'),
+('G9761'),
+('G9802'),
+('G9805'),
+('G9809'),
+('M1022'),
+('M1025'),
+('M1026'),
+('Z51.5');";
+sqlStatementNoLog($query);
