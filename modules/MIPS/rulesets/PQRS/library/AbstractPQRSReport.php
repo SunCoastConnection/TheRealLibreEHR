f5a0191e78539a18e5f5481d19259f77827cec0e
Art Eaton <Art@starfrontiers.org>
2019-07-11 02:03:00 -0400

    Update AbstractPQRSReport.php

--- modules/MIPS/rulesets/PQRS/library/AbstractPQRSReport.php
+++ modules/MIPS/rulesets/PQRS/library/AbstractPQRSReport.php
@@ -211,10 +211,10 @@ abstract class AbstractPQRSReport implements RsReportIF
                             }
                         }
                     }
-
+/////May redo NotMet like Exclusion/Exception above with a value of 5
                     $percentage = calculate_percentage( $pass_filt, $exclude_filt, $pass_targ );
                     $this->_resultsArray[]= new PQRSResult( $this->_rowRule, $title, $populationCriteria->getTitle(),
-                        $totalPatients, $pass_filt, $exclude_filt, $pass_targ, $percentage );
+                        $totalPatients, $pass_filt, $exclude_filt, $pass_targ, $pass_notmet, $percentage );
                 }
             }
         }
