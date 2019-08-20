cc99dacc058fcff27f81e2cf39467bc6769793c8
Elizabeth <asobingai@gmail.com>
2019-08-20 17:36:47 +0100

    resolve merge conflict

--- interface/forms/education/new.php
+++ interface/forms/education/new.php
@@ -47,29 +47,16 @@ $returnurl = 'encounter_top.php';
 			<thead>
 				<tr>
 				    <th>Topic #</th>
-<<<<<<< HEAD
-				    <th>Education Topic <span><a href="#" ><span title="add topic" onclick="addIssue()" style="color: #337ab7" class="glyphicon glyphicon-plus-sign"></span></a></span></th> 
-=======
 				    <th>Education Topic <span><a href="#" ><span title="add topic" onclick="addTopic()" style="color: #337ab7" class="glyphicon glyphicon-plus-sign"></span></a></span></th> 
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 				    <th>Learners </th> 
 				    <th>Remarks </th> 
 		 	 	</tr>
 			</thead>
-
-<<<<<<< HEAD
-			<tbody class="addIssue">
-				<tr id="row_1">
-				  	<td>
-				  		<mark>1</mark>
-				  		<a href="#" ><span style="color: #337ab7" onclick="addIntervention(1)" class="glyphicon glyphicon-plus-sign" title="Add Learner"></span> </a>
-=======
 			<tbody class="addTopic">
 				<tr id="row_1">
 				  	<td>
 				  		<mark>1</mark>
 				  		<a href="#" ><span style="color: #337ab7" onclick="addLearner(1)" class="glyphicon glyphicon-plus-sign" title="Add Learner"></span> </a>
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 				  		<a href="#"><span style="color: red" onclick="inactivateLearner(1)" class="glyphicon glyphicon-ban-circle" title="inactivate learner"></span> </a> 
 				  	</td>
 				  	<td><input type="text" name="topic[]" placeholder="Education topic"></td>
@@ -86,15 +73,6 @@ $returnurl = 'encounter_top.php';
 							   <th>Further interventions Needed</th>
 							</tr>
 				      	</thead>
-<<<<<<< HEAD
-				      	<tbody class="intervention_1">
-				      		<tr>
-					          <td><textarea name="intervention_1[]"></textarea></td>
-					          <td><textarea name="outcome_1[]"></textarea></td>
-					          <td><textarea name="goal_1[]"></textarea></td>
-					          <td><textarea name="progress_1[]"></textarea></td>  
-					          <td><textarea name="progress_1[]"></textarea></td>  
-=======
 				      	<tbody class="learner_1">
 				      		<tr>
 					          <td><textarea name="learners_1[]"></textarea></td>
@@ -102,7 +80,6 @@ $returnurl = 'encounter_top.php';
 					          <td><textarea name="response_1[]"></textarea></td>
 					          <td><textarea name="method_1[]"></textarea></td>  
 					          <td><textarea name="interventions_1[]"></textarea></td>  
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 					        </tr>
 				      	</tbody>   
 				      </table>
@@ -112,47 +89,28 @@ $returnurl = 'encounter_top.php';
 			</tbody>
 		  
 		</table>
-<<<<<<< HEAD
-		<textarea hidden="hidden"   name="Interventions"></textarea>
-		<textarea hidden="hidden" name="Outcome"></textarea>
-		<textarea hidden="hidden" name="Goal"></textarea>
-		<textarea hidden="hidden"  name="Progress" ></textarea>
-=======
 		<textarea hidden="hidden"   name="learners"></textarea>
 		<textarea hidden="hidden" name="readiness"></textarea>
 		<textarea hidden="hidden" name="response"></textarea>
 		<textarea hidden="hidden"  name="method" ></textarea>
 		<textarea hidden="hidden"  name="interventions" ></textarea>
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 
 	</form>
 
 	<script type="text/javascript">
 		var count = 1; 
-<<<<<<< HEAD
-		function addIntervention(currentIssueNumber ) {
-=======
 		function addLearner(currentIssueNumber ) {
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 			let intervention = createInterventionHtml(currentIssueNumber);
 			$(`.intervention_${currentIssueNumber}`).prepend(intervention);
 		}
 		
-<<<<<<< HEAD
-		function addIssue() {
-			let newIssue = newIssueHtml();
-			$('.addIssue').prepend(newIssue);
 
-		}
-		function deactivateIssue(issueNumber) {
-=======
 		function addTopic() {
 			let newIssue = newIssueHtml();
 			$('.addTopic').prepend(newIssue);
 
 		}
 		function inactivateLearner(issueNumber) {
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 			$(`.status_${issueNumber}`).attr("value", "0");
 			$(`#row_${issueNumber}`).css("background-color", "#c5c5bc");
 			$(`#row_${issueNumber}`).find("input, textarea").attr("readonly","readonly");
@@ -162,37 +120,16 @@ $returnurl = 'encounter_top.php';
 			return `<tr id="row_${count}">
 					  	<td>
 					  		<mark>${count}</mark>
-<<<<<<< HEAD
-					  		<a href="#" ><span style="color: #337ab7" onclick="addIntervention(${count})" class="glyphicon glyphicon-plus-sign" title="Add Interventions"></span> </a>
-					  		<a href="#"><span style="color: red" onclick="deactivateIssue(${count})" class="glyphicon glyphicon-ban-circle" title="Deactivate Issue"></span> </a> 
-					  	</td>
-					  	<td><input type="text" name="issue[]" placeholder="key issue"></td>
-=======
 					  		<a href="#" ><span style="color: #337ab7" onclick="addLearner(${count})" class="glyphicon glyphicon-plus-sign" title="Add Learners"></span> </a>
 					  		<a href="#"><span style="color: red" onclick="deactivateIssue(${count})" class="glyphicon glyphicon-ban-circle" title="Deactivate Topic"></span> </a> 
 					  	</td>
 					  	<td><input type="text" name="topic[]" placeholder="Education topic"></td>
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 					  	<input type="hidden" class="status_${count}" name="status[]" value="1">
 					  	<input type="hidden"  name="count[]" value="${count}">
 					  	<td>
 					      <table>
 					      	<thead>
 					      		<tr>
-<<<<<<< HEAD
-								   <th>Intervention</th>
-								   <th>Outcome</th> 
-								   <th>Goal</th>
-								   <th>Progress</th>
-								</tr>
-					      	</thead>
-					      	<tbody class="intervention_${count}">
-					      		<tr>
-						          	<td><textarea name="intervention_${count}[]"></textarea></td>
-								    <td><textarea name="outcome_${count}[]"></textarea></td>
-								    <td><textarea name="goal_${count}[]"></textarea></td>
-								    <td><textarea name="progress_${count}[]"></textarea></td>   
-=======
 								   <th>Learners</th>
 								   <th>Learner's Readiness for Education</th> 
 								   <th>Method of Education</th>
@@ -207,74 +144,27 @@ $returnurl = 'encounter_top.php';
 								    <td><textarea name="response_${count}[]"></textarea></td>
 								    <td><textarea name="method_${count}[]"></textarea></td>   
 								    <td><textarea name="interventions_${count}[]"></textarea></td>   
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 						        </tr>
 					      	</tbody>   
 					      </table>
 					    </td>
-<<<<<<< HEAD
-=======
 					    <td><textarea name="remark[]"></textarea></td>  
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 				  </tr>
 				  `
 		}
 
 		function createInterventionHtml(issue_number){
 			return `<tr>
-<<<<<<< HEAD
-					    <td><textarea name="intervention_${count}[]"></textarea></td>
-					    <td><textarea name="outcome_${count}[]"></textarea></td>
-					    <td><textarea name="goal_${count}[]"></textarea></td>
-					    <td><textarea name="progress_${count}[]"></textarea></td>  
-				    </tr>
-=======
 			          	<td><textarea name="learners_${count}[]"></textarea></td>
 					    <td><textarea name="readiness_${count}[]"></textarea></td>
 					    <td><textarea name="response_${count}[]"></textarea></td>
 					    <td><textarea name="method_${count}[]"></textarea></td>   
 					    <td><textarea name="interventions_${count}[]"></textarea></td>   
 			        </tr>
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 				 `
 		}
 
 		function beforeSubmit() {
-<<<<<<< HEAD
-			var intervention = new Array();
-			var outcome = new Array();
-			var goal = new Array();
-			var progress = new Array();
-			for (var i = 1; i <= count; i++){
-				let int = $(`textarea[name='intervention_${i}[]']`)
-              					.map(function(){return $(this).val();}).get();
-              	intervention.push(JSON.stringify(int));
-
-              	 let out = $(`textarea[name='outcome_${i}[]']`)
-              					.map(function(){return $(this).val();}).get();
-              	outcome.push(JSON.stringify(out));
-
-              	 let gol = $(`textarea[name='goal_${i}[]']`)
-              					.map(function(){return $(this).val();}).get();
-              	goal.push(JSON.stringify(gol));
-
-              	 let prog = $(`textarea[name='progress_${i}[]']`)
-              					.map(function(){return $(this).val();}).get();
-              	progress.push(JSON.stringify(prog));
-
-			}
-
-			objectIntervention = {"intervention": intervention};
-			objectOutcome = {"outcome": outcome};
-			objectGoal = {"goal": goal};
-			objectProgress = {"progress": progress};
-			console.log(JSON.stringify(objectIntervention));
-
-			$('textarea[name="Interventions"]').val(JSON.stringify(objectIntervention));
-            $('textarea[name="Outcome"]').val(JSON.stringify(objectOutcome));
-            $('textarea[name="Goal"]').val(JSON.stringify(objectGoal));
-            $('textarea[name="Progress"]').val(JSON.stringify(objectProgress));
-=======
 			var learner = new Array();
 			var readiness = new Array();
 			var response = new Array();
@@ -314,7 +204,6 @@ $returnurl = 'encounter_top.php';
             $('textarea[name="response"]').val(JSON.stringify(objectResponse));
             $('textarea[name="method"]').val(JSON.stringify(objectMethod));
             $('textarea[name="interventions"]').val(JSON.stringify(objectIntervention));
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
 
 		}
 	</script>
--- interface/forms/education/table.sql
+++ interface/forms/education/table.sql
@@ -8,17 +8,10 @@ CREATE TABLE IF NOT EXISTS `form_education` (
   `authorized` tinyint(4) DEFAULT NULL,
   `activity` tinyint(4) DEFAULT NULL,
   `topic` text,
-<<<<<<< HEAD
-  `learners` varchar(255) ,
-  `readiness`text,
-  `response` text  , 
-  `method` text ,
-=======
   `learners` text ,
   `readiness`text,
   `response` text, 
   `method` text,
   `interventions` text,
->>>>>>> 5b32339d05d752556de33636d1011f1c7a90b848
   `remark` text
 ) ENGINE=InnoDB;
